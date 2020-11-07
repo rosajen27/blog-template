61e69ebe7b0ab4b2a24bb3ae583f91d294bd01df
Jennifer Rosa <rosajen27@gmail.com>
2020-11-07 18:59:59 -0500

    create get request for all pages

--- app.js
+++ app.js
@@ -15,18 +15,26 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/", function(req, res) {
+  res.render("home", {homeParagraph: homeStartingContent});
+});
 
+app.get("/about", function(req, res) {
+  res.render("about", {aboutParagraph: aboutContent});
+});
 
+app.get("/contact", function(req, res) {
+  res.render("contact", {contactParagraph: contactContent});
+});
 
+app.get("/compose", function(req, res) {
+  res.render("compose");
+});
 
-
-
-
-
-
-
-
-
+app.post("/compose", function (req, res) {
+  console.log(req.body.entryTitle);
+  console.log(req.body.entryBody);
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
