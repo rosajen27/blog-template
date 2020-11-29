91bf5f2ea380cf7ec3e17d3ba39769f56f5f68e0
Jennifer Rosa <rosajen27@gmail.com>
2020-11-28 19:12:50 -0500

    create a new post document using mongoose model, save the document to your database instead of pushing to the posts array

--- app.js
+++ app.js
@@ -3,9 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const _ = require("lodash");
-// install mongoose
 const mongoose = require("mongoose");
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -18,18 +17,16 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// connect to blogDB database
 mongoose.connect("mongodb://localhost:27017/blogDB", { useNewUrlParser: true });
-
-let posts = [];
 
 const postSchema = {
   title: String,
   content: String
 };
 
-// mongoose model using postSchema to degine posts collections
 const Post = mongoose.model("Post", postSchema);
+
+let posts = [];
 
 app.get("/", function (req, res) {
   res.render("home", {
@@ -38,25 +35,19 @@ app.get("/", function (req, res) {
   });
 });
 
-app.get("/about", function (req, res) {
-  res.render("about", { aboutContent: aboutContent });
-});
-
-app.get("/contact", function (req, res) {
-  res.render("contact", { contactContent: contactContent });
-});
 
 app.get("/compose", function (req, res) {
   res.render("compose");
 });
 
 app.post("/compose", function (req, res) {
-  const post = {
+
+  const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody
-  };
+  });
 
-  posts.push(post);
+  post.save();
 
   res.redirect("/");
 
@@ -76,6 +67,14 @@ app.get("/posts/:postName", function (req, res) {
     }
   });
 
+});
+
+app.get("/about", function (req, res) {
+  res.render("about", { aboutContent: aboutContent });
+});
+
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactContent: contactContent });
 });
 
 app.listen(3000, function () {
