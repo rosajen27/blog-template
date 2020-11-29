54a1689ea43d2720c6ec395d9bf8716810fe5196
Jennifer Rosa <rosajen27@gmail.com>
2020-11-28 19:38:59 -0500

    add comments

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 //jshint esversion:6
 
+// Requirements
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const mongoose = require("mongoose");
-const _ = require("lodash");
+const mongoose = require('mongoose');
 
+// Static Content on each webpage
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
@@ -17,16 +18,21 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// Connect to a new database called blogDB
 mongoose.connect("mongodb://localhost:27017/blogDB", { useNewUrlParser: true });
 
+// Create a new postSchema that contains a title and content
 const postSchema = {
   title: String,
   content: String
 };
 
+// Create a new mongoose model using the schema to define posts collection
 const Post = mongoose.model("Post", postSchema);
 
+// Find all the posts in the posts collection and render that in the home.ejs file
 app.get("/", function (req, res) {
+
   Post.find({}, function (err, posts) {
     res.render("home", {
       startingContent: homeStartingContent,
@@ -35,48 +41,54 @@ app.get("/", function (req, res) {
   });
 });
 
-
+// Render compose form/content on compose page
 app.get("/compose", function (req, res) {
   res.render("compose");
 });
 
+// Create a new post document using mongoose model
 app.post("/compose", function (req, res) {
-
   const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody
   });
 
-  post.save();
-
-  res.redirect("/");
-
+  // Save the document to database instead of pushing to the posts array
+  // Callback to the save method to only redirect to the home page once save is complete with no errors
+  post.save(function (err) {
+    if (!err) {
+      res.redirect("/");
+    }
+  });
 });
 
-app.get("/posts/:postName", function (req, res) {
-  const requestedTitle = _.lowerCase(req.params.postName);
-
-  posts.forEach(function (post) {
-    const storedTitle = _.lowerCase(post.title);
-
-    if (storedTitle === requestedTitle) {
-      res.render("post", {
-        title: post.title,
-        content: post.content
-      });
-    }
+// home.ejs file: change the href of the anchor tag based on the post id instead of the post name
+// and change the express route parameter to postId instead
+app.get("/posts/:postId", function (req, res) {
+  // constant to store the postId parameter value
+  const requestedPostId = req.params.postId;
+  // use the findOne() method to find the post with a matching id in the posts collection
+  Post.findOne({ _id: requestedPostId }, function (err, post) {
+    // matching post is found, render its title and content in the post.ejs page.
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
   });
 
 });
 
+// Render about content on About page
 app.get("/about", function (req, res) {
   res.render("about", { aboutContent: aboutContent });
 });
 
+// Render contact content on Contact page
 app.get("/contact", function (req, res) {
   res.render("contact", { contactContent: contactContent });
 });
 
+// Server
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
